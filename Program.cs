ab6a790cac8d5314d48bdce476399ad1b50083ef
Colton Hanson <colton.hanson@growthzone.com>
2019-08-03 11:37:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MerlinAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
